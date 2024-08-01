e83d51e7260d30fdf4114b3bc0612af098ca111e
Andriana-Petrenko <144230812+Andriana-Petrenko@users.noreply.github.com>
2024-08-01 13:44:49 +0300

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import { env } from './utils/env.js';
+import rootRouter from './routers/index.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import cookieParser from 'cookie-parser';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
+
+const PORT = Number(env('PORT', '3001'));
+
+export const setupServer = () => {
+    
+    const app = express();
+
+    app.use('/api-docs', swaggerDocs());
+
+    app.use(cors());
+    
+    app.use(express.json());
+    
+    app.use(pino({
+        transport: {
+            target: 'pino-pretty',
+        },
+    })
+        ,);
+    
+    app.use(cookieParser());
+
+    app.use(rootRouter);
+    
+    app.use('*', notFoundHandler);
+    
+    app.use(errorHandler);
+
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+    
+};
+
+
